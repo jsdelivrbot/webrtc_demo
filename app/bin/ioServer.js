e831ec3f3addf208f268f121c782c7a067d8961b
cissiesee <496406128@qq.com>
2017-03-01 17:08:08 +0800

    mongodb

--- app/bin/ioServer.js
+++ app/bin/ioServer.js
@@ -1,23 +1,97 @@
 var server = require('./server');
 var _ = require('lodash');
 
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+ 
+// Connection URL 
+var url = 'mongodb://localhost:27017/myproject';
+// Use connect method to connect to the Server 
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+
+    insertDocuments(db, function() {
+        updateDocument(db, function() {
+            deleteDocument(db, function() {
+                findDocuments(db, function() {
+                    db.close();
+                });
+            });
+        });
+    });
+});
+
+var insertDocuments = function(db, callback) {
+    // Get the documents collection 
+    var collection = db.collection('documents');
+    // Insert some documents
+    collection.insertMany([
+        {a : 1}, {a : 2}, {a : 3}
+    ], function(err, result) {
+        assert.equal(err, null);
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log("Inserted 3 documents into the document collection");
+        callback(result);
+    });
+}
+
+var updateDocument = function(db, callback) {
+  // Get the documents collection 
+  var collection = db.collection('documents');
+  // Update document where a is 2, set b equal to 1 
+  collection.updateOne({ a : 2 }
+    , { $set: { b : 1 } }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Updated the document with the field a equal to 2");
+    callback(result);
+  });  
+}
+
+var deleteDocument = function(db, callback) {
+  // Get the documents collection 
+  var collection = db.collection('documents');
+  // Insert some documents 
+  collection.deleteOne({ a : 3 }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Removed the document with the field a equal to 3");
+    callback(result);
+  });
+}
+
+var findDocuments = function(db, callback) {
+  // Get the documents collection 
+  var collection = db.collection('documents');
+  // Find some documents 
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    //assert.equal(2, docs.length);
+    console.log("Found the following records");
+    console.dir(docs);
+    callback(docs);
+  });
+}
+
 var ioServer = require('socket.io')(server);
 
 var users = [];
 
 ioServer.on('connection', function(socket) {
-	console.log(users);
-	// for(key in socket.client) {
-	// 	console.log(key);
-	// }
-	ioServer.to(socket.id).emit('init', users);
-	socket.on('peer_open', function(user) {
-		if(!_.find(users, {id: user.id})) {
-			users.push(user);
-			socket.broadcast.emit('broadcast', {type: 'peer_open', content: user});
-		}
-		console.log(users);
-	});
+    console.log(users);
+    // for(key in socket.client) {
+    //  console.log(key);
+    // }
+    ioServer.to(socket.id).emit('init', users);
+    socket.on('peer_open', function(user) {
+        if(!_.find(users, {id: user.id})) {
+            users.push(user);
+            socket.broadcast.emit('broadcast', {type: 'peer_open', content: user});
+        }
+        console.log(users);
+    });
 });
 
 module.exports = ioServer;
